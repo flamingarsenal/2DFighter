113e96b7729ef25d3e65a62d2ff27d26d3600e2a
flamingarsenal <hotcoder.uzair@gmail.com>
2026-03-02 13:33:56 +0600

    update 2 Mar 26

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS)) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS)) {
         SDL_Window* window = SDL_CreateWindow("2DFighter", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 800, 0);
         SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
